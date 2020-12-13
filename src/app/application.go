e0789e44896c58af26e8224deabab307be662c3f
Eric Ha <ericha@Erics-MBP.fios-router.home>
2020-12-12 20:20:01 -0600

    Check in after Section 6-27

--- src/app/application.go
+++ src/app/application.go
@@ -1,7 +1,6 @@
 package app
 
 import (
-	"github.com/ericha1981/bookstore_oauth-api/src/clients/cassandra"
 	"github.com/ericha1981/bookstore_oauth-api/src/domain/access_token"
 	"github.com/ericha1981/bookstore_oauth-api/src/http"
 	"github.com/ericha1981/bookstore_oauth-api/src/repository/db"
@@ -11,12 +10,6 @@ import (
 var router = gin.Default()
 
 func StartApplication()  {
-	session, dbErr := cassandra.GetSession()
-	if dbErr != nil {
-		panic(dbErr)
-	}
-	session.Close()
-
 	atHandler := http.NewHandler(access_token.NewService(db.NewRepository())) // Presentation/Web layer (http)
 	router.GET("/oauth/access_token/:access_token_id", atHandler.GetById)
 	router.POST("/oauth/access_token", atHandler.Create)
--- src/clients/cassandra/cassandra_db.go
+++ src/clients/cassandra/cassandra_db.go
@@ -5,16 +5,21 @@ import (
 )
 
 var (
-	cluster *gocql.ClusterConfig
+	session *gocql.Session
 )
 
 func init() {
 	// Connect to Cassandra cluster(not "database server")
-	cluster = gocql.NewCluster("127.0.0.1") // localhost
+	cluster := gocql.NewCluster("127.0.0.1") // localhost
 	cluster.Keyspace = "oauth"
 	cluster.Consistency = gocql.Quorum
+
+	var err error
+	if session, err = cluster.CreateSession(); err != nil {
+		panic(err)
+	}
 }
 
-func GetSession() (*gocql.Session, error) {
-	return cluster.CreateSession()
+func GetSession() *gocql.Session {
+	return session
 }
